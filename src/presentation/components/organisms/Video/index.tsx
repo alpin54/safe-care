af801e20dbac336c6d6f2c14eeacff7f3b9aeed7
alpin54 <alpinn189@gmail.com>
2026-03-06 00:41:21 +0700

    update

--- src/presentation/components/organisms/Video/index.tsx
+++ src/presentation/components/organisms/Video/index.tsx
@@ -7,14 +7,15 @@ const VideoSection: React.FC = () => {
 	return (
 		<section className={style.video} id="video">
 			<div className="container">
-				<iframe
-					className={style.videoPlayer}
-					src="https://player.vimeo.com/video/1170745973?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-					frameBorder="0"
-					allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-					referrerPolicy="strict-origin-when-cross-origin"
-					title="video"
-				></iframe>
+				<div className={style.videoPlayer}>
+					<iframe
+						src="https://player.vimeo.com/video/1170745973?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+						frameBorder="0"
+						allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+						referrerPolicy="strict-origin-when-cross-origin"
+						title="video"
+					/>
+				</div>
 				<script src="https://player.vimeo.com/api/player.js"></script>
 			</div>
 		</section>
