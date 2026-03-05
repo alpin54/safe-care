c71140887fe57233e9eb12b1582d897be1934584
alpin54 <alpinn189@gmail.com>
2026-03-06 00:14:47 +0700

    update

--- src/presentation/components/organisms/Video/index.tsx
+++ src/presentation/components/organisms/Video/index.tsx
@@ -1,19 +1,18 @@
 "use client";
 
-// -- core
 import React from "react";
-
-// -- style
 import style from "./style.module.scss";
 
-const FeatureSection: React.FC = () => {
+const VideoSection: React.FC = () => {
 	return (
 		<section className={style.video} id="video">
 			<div className="container">
-				{/* <h2 className={style.title}>
-					Tonton Video Edukasi TB untuk Kenali Gejala dan Pencegahannya
-				</h2> */}
-				<video controls className={style.videoPlayer}>
+				<video
+					controls
+					className={style.videoPlayer}
+					preload="metadata"
+					poster="/bg/hero-banner.png"
+				>
 					<source src="/video/video.mp4" type="video/mp4" />
 				</video>
 			</div>
@@ -21,4 +20,4 @@ const FeatureSection: React.FC = () => {
 	);
 };
 
-export default FeatureSection;
+export default VideoSection;
