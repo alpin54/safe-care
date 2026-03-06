3435cdd8d91bbcf6434b4bde8f14333d2a8fde22
alpin54 <alpinn189@gmail.com>
2026-03-06 09:33:21 +0700

    update contact

--- src/presentation/components/organisms/Video/index.tsx
+++ src/presentation/components/organisms/Video/index.tsx
@@ -9,14 +9,14 @@ const VideoSection: React.FC = () => {
 			<div className="container">
 				<div className={style.videoPlayer}>
 					<iframe
-						src="https://player.vimeo.com/video/1170745973?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+						src="https://player.vimeo.com/video/1170865979?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
 						frameBorder="0"
 						allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
 						referrerPolicy="strict-origin-when-cross-origin"
-						title="video"
+						title="VID-20260305-WA0021(1)"
 					/>
 				</div>
-				<script src="https://player.vimeo.com/api/player.js"></script>
+				<script src="https://player.vimeo.com/api/player.js" />
 			</div>
 		</section>
 	);
